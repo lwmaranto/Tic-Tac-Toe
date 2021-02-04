f66f05e8b6c6080444fd9cbfa9da83ab38a6dd64
lwmaranto <laurawmaranto@gmail.com>
2021-02-03 22:09:36 -0700

    methods outline and printBoard

--- game.js
+++ game.js
@@ -7,4 +7,19 @@ class Board {
     ];
     this.turn = ["X", "O"][Math.floor(Math.random() * 2)];
   }
+  printBoard() {
+    let result = "";
+    for (let i = 0; i < this.board.length; i++) {
+      for (let j = 0; j < this.board[i].length; j++) {
+        result = result + this.board[i][j];
+      }
+      result = result + "\n";
+    }
+    return result;
+  }
+  printLegend() {}
+  setMove() {}
+  whoseTurn() {}
+  isFull() {}
+  whoWon() {}
 }
